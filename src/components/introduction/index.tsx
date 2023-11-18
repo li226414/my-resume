e831295089dc7d94cde03d843a71c0cc9bbfe1c2
栗嘉男 <574980606@qq.com>
2023-11-19 05:01:56 +0800

    fix: fix

--- src/components/introduction/index.tsx
+++ src/components/introduction/index.tsx
@@ -5,11 +5,10 @@ import styles from "./index.module.scss";
 export const Introduction: React.FC = () => {
   return (
     <p className={clsx(styles.introduction)}>
-      四年前端经验，拥有 2B/2C 多个项目经验，熟悉 React，参与过 React 官方文档翻译工作，拥有丰富的
-      React + TS 实践经验，积极参与行业内优秀前端技术框架开源共建，目前是 AntDesign 全球 49
-      名团队成员之一、ahooks 团队成员之一，活跃于 GitHub 开源社区，给
-      next.js、vite、tdesign、react-query 等众多知名大型开源库贡献过
-      PR，乐于分享，对新技术保持高度敏感，喜欢钻研前沿优秀技术，喜欢研究一些优秀框架的源码，并梳理沉淀成技术文章，和团队成员一起成长。有较强的技术判断力和业务敏锐度。
+      四年前端经验，拥有 2B/2C 多个项目经验，拥有丰富的 React + TS
+      实践经验，积极参与行业内优秀前端技术框架开源共建，目前是 AntDesign 全球 49
+      名核心成员之一、ahooks
+      核心成员之一，乐于分享，对新技术保持高度敏感，喜欢钻研前沿优秀技术，喜欢研究一些优秀框架的源码，并梳理沉淀成技术文章，和团队成员一起成长，有较强的技术判断力和业务敏锐度。
     </p>
   );
 };
--- src/components/skill-list/index.tsx
+++ src/components/skill-list/index.tsx
@@ -2,7 +2,14 @@ import React from "react";
 import clsx from "clsx";
 import styles from "./index.module.scss";
 
-const dataLst = [1, 2, 3];
+const dataLst = [
+  "antd 团队成员、ahooks 团队成员，活跃于 GitHub 开源社区，给 Next.js、Vite 等众多知名大型开源库贡献过 PR",
+  "熟悉 React 及其原理，参与过 React 官方文档翻译工作，熟悉 TypeScript，拥有丰富的 React + TS 实战经验",
+  "熟悉前端性能优化的实现，例如代码优化、打包优化、资源优化，能结合实际业务场景进行优化",
+  "熟悉 Webpack / Vite 等打包工具的基本配置， 能够对以上工具进行二次封装、基于以上工具搭建通用的开发环境",
+  "熟悉 prettier / eslint 基本配置，有良好且严格的编码习惯，唯客户论，实用主义者",
+  "熟悉代码开发到上线全流程，对协同开发，分支管理，项目配置等都有较深刻的最佳实践",
+];
 
 export const SkillList: React.FC = () => {
   return (
